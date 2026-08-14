c26b5c283824256bcb147ad237ce4c2e8b349252
Tarek Mohamed <tarek.mohamed@oour-edu.net>
2026-08-14 18:30:51 +0300

    add initial implementation of memory queue; enhance enqueue and shutdown methods, and update error handling

--- internal/queue/memory.go
+++ internal/queue/memory.go
@@ -1,3 +1,48 @@
+// package queue
+
+// import (
+// 	"context"
+// 	"sync"
+
+// 	"github.com/A1ztec/go-job/internal/job"
+// )
+
+// type memoryQueue struct {
+// 	jobs   chan *job.Job
+// 	mu     sync.Mutex
+// 	Closed bool
+// }
+
+// func NewMemoryQueue(bufferSize int) *memoryQueue {
+// 	return &memoryQueue{
+// 		jobs: make(chan *job.Job, bufferSize),
+// 	}
+// }
+
+// func (mq *memoryQueue) Enqueue(ctx context.Context, j *job.Job) error {
+// 	select {
+// 	case mq.jobs <- j:
+// 		return nil
+// 	case <-ctx.Done():
+// 		return ctx.Err()
+// 	}
+// }
+
+// func (mq *memoryQueue) Dequeue(ctx context.Context) (*job.Job, error) {
+// 	select {
+// 	case j := <-mq.jobs:
+// 		return j, nil
+// 	case <-ctx.Done():
+// 		return nil, ctx.Err()
+// 	}
+// }
+
+// func (mq *memoryQueue) Shutdown(ctx context.Context) error {
+// 	mu.lock()
+// 	close(mq.jobs)
+// 	return nil
+// }
+
 package queue
 
 import (
@@ -10,7 +55,7 @@ import (
 type memoryQueue struct {
 	jobs   chan *job.Job
 	mu     sync.Mutex
-	Closed bool
+	closed bool
 }
 
 var _ Queue = (*memoryQueue)(nil)
@@ -22,6 +67,12 @@ func NewMemoryQueue(bufferSize int) *memoryQueue {
 }
 
 func (mq *memoryQueue) Enqueue(ctx context.Context, j *job.Job) error {
+	mq.mu.Lock()
+	if mq.closed {
+		mq.mu.Unlock()
+		return ErrQueueClosed
+	}
+	mq.mu.Unlock()
 	select {
 	case mq.jobs <- j:
 		return nil
@@ -42,11 +93,10 @@ func (mq *memoryQueue) Dequeue(ctx context.Context) (*job.Job, error) {
 func (mq *memoryQueue) Shutdown(ctx context.Context) error {
 	mq.mu.Lock()
 	defer mq.mu.Unlock()
-
-	if mq.Closed {
+	if mq.closed {
 		return nil
 	}
-	mq.Closed = true
 	close(mq.jobs)
+	mq.closed = true
 	return nil
 }
--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -2,8 +2,14 @@ package queue
 
 import (
 	"context"
+	"errors"
 
 	"github.com/A1ztec/go-job/internal/job"
+)
+
+var (
+	ErrQueueClosed = errors.New("queue is closed")
+	ErrQueueEmpty  = errors.New("queue is empty")
 )
 
 type Queue interface {
