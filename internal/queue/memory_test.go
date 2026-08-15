913dcf54f6b6a34f927294112f7ab964a994b02e
Tarek Mohamed <tarek.mohamed@oour-edu.net>
2026-08-16 00:38:31 +0300

    add test for context handling in Dequeue method of MemoryQueue

--- internal/queue/memory_test.go
+++ internal/queue/memory_test.go
@@ -3,6 +3,7 @@ package queue
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/A1ztec/go-job/internal/job"
 )
@@ -23,4 +24,17 @@ func TestMemoryQueue_EnqueueDequeue(t *testing.T) {
 	if got.ID != j.ID {
 		t.Errorf("got job ID %q, want %q", got.ID, j.ID)
 	}
+}
+
+func TestMemoryQueue_DequeueRespectsContext(t *testing.T) {
+	q := NewMemoryQueue(100)
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Microsecond)
+	defer cancel()
+
+	_, err := q.Dequeue(ctx)
+
+	if err == nil {
+		t.Fatalf("i was expecting and error got nil")
+	}
+
 }
