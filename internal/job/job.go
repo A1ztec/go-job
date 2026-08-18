f4dac41c58d5d138f81e7234fc049fa725d6c79c
Tarek Mohamed <tarek.mohamed@oour-edu.net>
2026-08-18 22:30:20 +0300

    refactor: enhance Job creation with options for MaxAttempts and Backoff

--- internal/job/job.go
+++ internal/job/job.go
@@ -15,6 +15,20 @@ const (
 	StatusFailed     Status = "failed"
 )
 
+type Options func(*Job)
+
+func WithMaxAttempts(Ma int) Options {
+	return func(j *Job) {
+		j.MaxAttempts = Ma
+	}
+}
+
+func WithBackoff(t time.Duration) Options {
+	return func(j *Job) {
+		j.BackOff = t
+	}
+}
+
 type Job struct {
 	ID          string
 	Type        string
@@ -23,17 +37,23 @@ type Job struct {
 	MaxAttempts int
 	Status      Status
 	Attempts    int
+	BackOff     time.Duration
 }
 
-func New(t string, payload []byte) *Job {
-	return &Job{
+func New(t string, payload []byte, options ...Options) *Job {
+	j := &Job{
 		ID:          uuid.NewString(),
 		Type:        t,
 		Payload:     payload,
 		Status:      StatusPending,
-		MaxAttempts: 3,
+		MaxAttempts: 0,
 		CreatedAt:   time.Now(),
+		BackOff:     0,
 	}
+	for _, o := range options {
+		o(j)
+	}
+	return j
 }
 
 func (j *Job) CanRetry() bool {
